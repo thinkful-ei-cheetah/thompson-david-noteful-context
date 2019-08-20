396cf478d3ef681c043e9963010dccfd516723a9
David Nordeen <davidnordeen@gmail.com>
2019-08-19 19:29:40 -0700

    almost done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: 'http://fierce-reaches-60231.herokuapp.com/'
+  API_ENDPOINT: 'https://fierce-reaches-60231.herokuapp.com'
+  // API_ENDPOINT: 'http://localhost:8000/'
 }
